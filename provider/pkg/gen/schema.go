b2104e0d79ce0cd35ca48e987913711cc0ea3f66
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2024-05-03 09:45:00 -0700

    Regen schema

--- provider/pkg/gen/schema.go
+++ provider/pkg/gen/schema.go
@@ -137,8 +137,11 @@ func PulumiSchema(openapiDoc openapi3.T) (pschema.PackageSpec, openapigen.Provid
 	})
 
 	metadata := openapigen.ProviderMetadata{
-		ResourceCRUDMap: providerMetadata.ResourceCRUDMap,
-		AutoNameMap:     providerMetadata.AutoNameMap,
+		ResourceCRUDMap:  providerMetadata.ResourceCRUDMap,
+		AutoNameMap:      providerMetadata.AutoNameMap,
+		SDKToAPINameMap:  providerMetadata.SDKToAPINameMap,
+		APIToSDKNameMap:  providerMetadata.APIToSDKNameMap,
+		PathParamNameMap: providerMetadata.PathParamNameMap,
 	}
 	return pkg, metadata, updatedOpenAPIDoc
 }
